2f8936bff664ae73f56600c525e937e36588820c
Sagar Annaldas <sagar@billeasy.in>
2022-12-06 18:41:39 +0530

    1. Created models for customer, contacts, authentication

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -5,3 +5,26 @@ class SliderObject {
 
   SliderObject(this.title, this.subTitle, this.image);
 }
+
+class Customer {
+  late String id;
+  late String name;
+  late int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  late String email;
+  late String phone;
+  late String link;
+
+  Contacts(this.email, this.phone, this.link);
+}
+
+class Authentication {
+  late Customer customer;
+  late Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
