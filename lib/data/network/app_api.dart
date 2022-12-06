fcf4c3dd7a00167bedbf87ff667edff63e2c40a6
Sagar Annaldas <sagar@billeasy.in>
2022-12-06 18:35:35 +0530

    1. Created app_api.dart abstract class to create api's and app_api.g.dart 2. Created constant.dart

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,16 @@
+import 'package:retrofit/http.dart';
+import 'package:tut_app_flutter/app/constant.dart';
+import 'package:dio/dio.dart';
+import '../responses/responses.dart';
+part 'app_api.g.dart';
+
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio,{String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login();
+
+
+}
