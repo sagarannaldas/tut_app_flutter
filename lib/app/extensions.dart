73fcdfb22b66a81eb9c66d9d674885ef74cb4e5d
Sagar Annaldas <sagar@billeasy.in>
2022-12-06 18:53:31 +0530

    1. Created extensions.dart for extension function

---
+++ lib/app/extensions.dart
@@ -0,0 +1,21 @@
+// extension on string
+extension NonNullString on String? {
+  String orEmpty() {
+    if (this == null) {
+      return "";
+    } else {
+      return this!;
+    }
+  }
+}
+
+// extension on integer
+extension NonNullInt on int? {
+  int orZero() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
