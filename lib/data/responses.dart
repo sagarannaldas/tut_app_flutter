fd4736275da8cc0a69b26d78740b0b7878750104
Sagar Annaldas <sagar@billeasy.in>
2022-12-06 18:09:00 +0530

    1. Created Json serialization in responses.dart and responses.g.dart

--- lib/data/responses.dart
+++ lib/data/responses.dart
@@ -1,5 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
+part 'responses.g.dart';
+
 @JsonSerializable()
 class BaseResponse {
   @JsonKey(name: "status")
@@ -8,6 +10,7 @@ class BaseResponse {
   String? message;
 }
 
+@JsonSerializable()
 class CustomerResponse {
   @JsonKey(name: "id")
   String? id;
@@ -15,8 +18,18 @@ class CustomerResponse {
   String? name;
   @JsonKey(name: "numOfNotifications")
   int? numOfNotifications;
+
+  CustomerResponse(this.id, this.name, this.numOfNotifications);
+
+  // from json
+  factory CustomerResponse.fromJson(Map<String, dynamic> json) =>
+      _$CustomerResponseFromJson(json);
+
+  // to json
+  Map<String, dynamic> toJson() => _$CustomerResponseToJson(this);
 }
 
+@JsonSerializable()
 class ContactsResponse {
   @JsonKey(name: "phone")
   String? phone;
@@ -24,6 +37,15 @@ class ContactsResponse {
   String? link;
   @JsonKey(name: "email")
   String? email;
+
+  ContactsResponse(this.phone, this.link, this.email);
+
+  // from json
+  factory ContactsResponse.fromJson(Map<String, dynamic> json) =>
+      _$ContactsResponseFromJson(json);
+
+  // to json
+  Map<String, dynamic> toJson() => _$ContactsResponseToJson(this);
 }
 
 @JsonSerializable()
@@ -32,4 +54,13 @@ class AuthenticationResponse extends BaseResponse {
   CustomerResponse? customer;
   @JsonKey(name: "contacts")
   ContactsResponse? contacts;
+
+  AuthenticationResponse(this.customer, this.contacts);
+
+  // from json
+  factory AuthenticationResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthenticationResponseFromJson(json);
+
+  // to json
+  Map<String, dynamic> toJson() => _$AuthenticationResponseToJson(this);
 }
