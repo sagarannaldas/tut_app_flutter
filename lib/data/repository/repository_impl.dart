67dd5e062c14832dd4ab3422a7971e07692fd071
Sagar Annaldas <sagar@billeasy.in>
2022-12-08 16:43:44 +0530

    1. Added api error handling implementation in repository_impl.dart

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:tut_app_flutter/data/data_source/remote_data_source.dart';
 import 'package:tut_app_flutter/data/mapper/mapper.dart';
+import 'package:tut_app_flutter/data/network/error_handler.dart';
 import 'package:tut_app_flutter/data/network/failure.dart';
 import 'package:tut_app_flutter/data/network/network_info.dart';
 import 'package:tut_app_flutter/data/request/request.dart';
@@ -11,23 +12,28 @@ class RepositoryImpl extends Repository {
   late final RemoteDataSource _remoteDataSource;
   late final NetworkInfo _networkInfo;
 
-  RepositoryImpl(this._networkInfo,this._remoteDataSource);
+  RepositoryImpl(this._networkInfo, this._remoteDataSource);
 
   @override
-  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest) async {
-    if(await _networkInfo.isConnected) {
-      final response = await _remoteDataSource.login(loginRequest);
-      if(response.status == 0) {
-        // success
-        return Right(response.toDomain());
-      } else {
-        // failure
-        return Left(Failure(409,response.message?? "we have biz error logic from API side"));
+  Future<Either<Failure, Authentication>> login(
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final response = await _remoteDataSource.login(loginRequest);
+        if (response.status == ApiInternalStatus.SUCCESS) {
+          // success
+          return Right(response.toDomain());
+        } else {
+          // failure
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
       }
     } else {
       // no connection
-      return Left(Failure(501, "please check your internet connection"));
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
-
 }
