c1c3fe2666e63993b9a96b6bdf6305f49b46a59b
Sagar Annaldas <sagar@billeasy.in>
2022-12-08 17:52:28 +0530

    1. Created language_manager.dart 2. Created app_preferences.dart 3. Added library shared_preferences

---
+++ lib/presentation/resources/language_manager.dart
@@ -0,0 +1,15 @@
+enum LanguageType { ENGLISH, HINDI }
+
+const String ENGLISH = "en";
+const String HINDI = "hi";
+
+extension LanguageTypeExtension on LanguageType {
+  String getValue() {
+    switch (this) {
+      case LanguageType.ENGLISH:
+        return ENGLISH;
+      case LanguageType.HINDI:
+        return HINDI;
+    }
+  }
+}
