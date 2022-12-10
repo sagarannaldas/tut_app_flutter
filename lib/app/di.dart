cc7a2e2617e6436033807d21f69965a95182776e
Sagar Annaldas <sagar@billeasy.in>
2022-12-10 13:13:41 +0530

    1. Added dependency instance object in di.dart 2. Initialization of app module dependency in main.dart

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,6 +1,14 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tut_app_flutter/app/app_preferences.dart';
+import 'package:tut_app_flutter/data/network/app_api.dart';
+import 'package:tut_app_flutter/data/network/dio_factory.dart';
+import 'package:tut_app_flutter/data/repository/repository_impl.dart';
+import 'package:tut_app_flutter/domain/repository/repository.dart';
+
+import '../data/data_source/remote_data_source.dart';
+import '../data/network/network_info.dart';
 
 final instance = GetIt.instance;
 
@@ -9,7 +17,28 @@ Future<void> initAppModule() async {
 
   // shared preference instance
   instance.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+
   // app preference instance
   instance.registerLazySingleton<AppPreferences>(
       () => AppPreferences(instance<SharedPreferences>()));
+
+  // network info instance
+  instance.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(DataConnectionChecker()));
+
+  // dio factory
+  instance.registerLazySingleton<DioFactory>(
+      () => DioFactory(instance<AppPreferences>()));
+
+  // app service client
+  final dio = await instance<DioFactory>().getDio();
+  instance.registerLazySingleton<AppServiceClient>(() => AppServiceClient(dio));
+
+  // remote data source
+  instance.registerLazySingleton<RemoteDataSource>(
+      () => RemoteDataSourceImpl(instance<AppServiceClient>()));
+
+  // repository
+  instance.registerLazySingleton<Repository>(
+      () => RepositoryImpl(instance(), instance()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tut_app_flutter/app/di.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  // init dependency module in app
+  WidgetsFlutterBinding
+      .ensureInitialized(); // ensure to init all instance for legacy lib which can be null
+  await initAppModule();
   runApp(MyApp());
 }
